e76d9e3b262786bc2a452b42410d9066358ae8eb
Vicheese369 <vichekatho4@gmail.com>
2025-12-01 14:56:55 +0700

    feat: print book

--- utils/book.hpp
+++ utils/book.hpp
@@ -2,6 +2,10 @@
 #define book_hpp
 #include "shared.hpp"
 
+
+//Maybe implemnet a binary search tree here so we can store it alphabetically automatically
+//Also probably change the booknode data into a struct because this is fucking ugly as shit
+
 struct BookData{
     string bookID;
     string bookTitle;
@@ -56,13 +60,12 @@ class BookList{
                 node->bookData.availableCopies = bookData.availableCopies;
                 node->bookData.availability = bookData.availability;
             }
-            size++;
 
             return node;
         }
 
         BookNode *searchByIDHelper(BookNode *node, string id){
-            if(node == nullptr){
+            if(node == NULL){
                 return nullptr;
             } else if (id == node->bookData.bookID){
                 return node;
@@ -72,91 +75,6 @@ class BookList{
                 return searchByIDHelper(node->left, id);
             }
         }
-
-        BookNode *getSuccessor(BookNode* curr){
-            curr = curr->right;
-            while(curr != nullptr && curr->left != nullptr){
-                curr = curr->left;
-            }
-            return curr;
-        }
-
-        BookNode *removeByIDHelper(BookNode *node, string id){
-            if(node == nullptr){
-                return nullptr;
-            } else if (node->bookData.bookID > id){
-                node->left = removeByIDHelper(node->left, id);
-            } else if (node->bookData.bookID < id){
-                node->right = removeByIDHelper(node->right, id);
-            } else {
-                if(node->left == nullptr){
-                    BookNode* temp = node->right;
-                    delete node;
-                    return temp;
-                }
-                if (node->right == nullptr){
-                    BookNode* temp = node->left;
-                    delete node;
-                    return temp;
-                }
-            }
-            BookNode* succ = getSuccessor(node);
-            node->bookData = succ->bookData;
-            node->right = removeByIDHelper(root->right, succ->bookData.bookID);
-            
-        }
-
-        vector<BookData> searchByAuthorHelper(BookNode *node, string inputAuthor){
-            vector<BookData> bookDataVector;
-            if(node != nullptr){
-                searchByAuthorHelper(node->left, inputAuthor);
-                if(inputAuthor == node->bookData.bookAuthor){
-                    bookDataVector.push_back(node->bookData);
-                }
-                searchByAuthorHelper(node->right, inputAuthor);
-            }
-            return bookDataVector;
-        }
-
-        vector<BookData> searchByTitleHelper(BookNode *node, string inputTitle){
-            vector<BookData> bookDataVector;
-            //Implement
-
-            return bookDataVector;
-        }
-
-        vector<BookData> preOrderHelper(BookNode *node){
-            vector<BookData> bookDataVector;
-            if(node != nullptr){
-                bookDataVector.push_back(node->bookData);
-                preOrderHelper(node->left);
-                preOrderHelper(node->right);
-            }
-            return bookDataVector;
-        }
-
-        vector<BookData> inOrderHelper(BookNode *node){
-            vector<BookData> bookDataVector;
-            if(node != nullptr){
-                inOrderHelper(node->left);
-                bookDataVector.push_back(node->bookData);
-                inOrderHelper(node->right);
-            }
-            return bookDataVector;
-        }
-
-        vector<BookData> postOrderHelper(BookNode *node){
-            vector<BookData> bookDataVector;
-            if(node != nullptr){
-                inOrderHelper(node->left);
-                inOrderHelper(node->right);
-                bookDataVector.push_back(node->bookData);
-            }
-            return bookDataVector;
-        }
-
-
-
     public:
         BookList(BookData bookData){
             root = new BookNode(bookData);
@@ -180,6 +98,11 @@ class BookList{
 
         }
 
+        //Remove entry by ID and reorder BST
+        void removeByID(string id){
+
+        }
+
         //Insert book into a BST pre sorted lexographically
         void insert(BookData bookData){
             root = insertHelper(root, bookData);
@@ -190,97 +113,46 @@ class BookList{
             searchByIDHelper(root, id);
         }
 
-
-        //Remove entry by ID and reorder BST
-        BookNode* removeByID(string id){
-            removeByIDHelper(root, id);
-        }
-
-        
-        //Save BST of books into db file
-        void saveBooksToFile(string filename){
-            ofstream file(filename);
-            if(!file.is_open()){
-                cout << "Error opening file " << filename << endl;
-                return;
-            }
-
-            vector<BookData> bookDataVector = inOrderHelper(root);
-            file << "index,book_id,title,author,isbn,total_copies,available_copies,availabilty" << 'endl'; 
-
-            int index = 1;
-            for(const BookData& itr : bookDataVector){
-                file << index << ','
-                    << itr.bookID << ','
-                    << itr.bookTitle << ','
-                    << itr.bookAuthor << ','
-                    << itr.isbn << ','
-                    << itr.totalCopies << ','
-                    << itr.availableCopies << ','
-                    << itr.availability << endl ;
-                index++;
-            }
-            
-            file.close();
-            cout << "Book data successfully saved to " << filename << endl;
-        }
         //Load books from db and insert into BST
-        void loadBooksFromFile(string filename){
-            bool firstLine = true;
+        void loadBooksFromDB(string filename){
 
-            ifstream file(filename);
-            if(!file.is_open()){
-                cout << "Error opening file " << filename << endl;
-                return;
-            }
-            
-            string line;
-            while(getline(file, line)){
-                if(firstLine){
-                    firstLine = false;
-                    continue;
-                }
+        }
 
-                istringstream iss(line);
-                int index;
-                string field;
-
-                BookData bookData;
-
-                getline(iss, field, ',');
-                try{
-                    index = stoi(field);
-                } catch(const exception& e){
-                    cerr << "Error converting index: " << e.what() << endl;
-                    continue;
-                }
-
-                string totalCopiesField;
-                string availableCopiesField;
-                string availabilityField;
-                
-                if(getline(iss, bookData.bookID, ',') &&
-                    getline(iss, bookData.bookTitle, ',') &&
-                    getline(iss, bookData.bookAuthor, ',') &&
-                    getline(iss, bookData.isbn, ',') &&
-                    getline(iss, totalCopiesField, ',') &&
-                    getline(iss, availableCopiesField, ',') &&     
-                    getline(iss, availabilityField, ',')){
-                        bookData.totalCopies = stoi(totalCopiesField);
-                        bookData.availableCopies = stoi(availableCopiesField);
-                        bookData.availability = solve(availabilityField);
-
-                        insert(bookData);
-                }
-            }
-
-            file.close();
+        //Save BST of books into db file
+        void saveBooksToDB(string filename){
 
         }
 
         //Prints the entire book list
         void print(){
-
+            void print() {
+                vector<BookData> bookDataVector = inOrderHelper(root);
+                if(bookDataVector.empty()) {
+                    cout << "No books available in the library." << endl;
+                    return;
+                }
+                cout << setw(5) << left << "No:" 
+                     << setw(15) << left << "Book ID" 
+                     << setw(30) << left << "Title" 
+                     << setw(25) << left << "Author" 
+                     << setw(15) << left << "ISBN" 
+                     << setw(10) << left << "Total" 
+                     << setw(10) << left << "Available" 
+                     << setw(10) << left << "Status" << endl;
+                int index = 1;
+                for(const auto& book : bookDataVector) {
+                    cout << setw(5) << left << index
+                         << setw(15) << left << book.bookID
+                         << setw(30) << left << book.bookTitle
+                         << setw(25) << left << book.bookAuthor
+                         << setw(15) << left << book.isbn
+                         << setw(10) << left << book.totalCopies
+                         << setw(10) << left << book.availableCopies
+                         << setw(10) << left << (book.availability ? "Yes" : "No") 
+                         << endl;
+                    index++;
+                }
+            }            
         }
 
         //Display and change tui i guess, ill do this one
@@ -307,7 +179,10 @@ class BookList{
 
         //Edit book by ID
         void edit(string inputID){
+        }
 
+        //Remove book by ID
+        void remove(string inputID){
         }
 
         //Search by title, create a new searchByTitleHelper
@@ -317,38 +192,6 @@ class BookList{
 
         //Search by author, create a new searchByAuthorHyper
         void searchByAuthor(string inputAuthorName){
-            int itemPerPage = 5;
-            int totalPage;
-            int totalItem;
-            int currentPage;
-            vector<BookData> bookDataVector = searchByAuthorHelper(root, inputAuthorName);
-
-            vector<BookData>::iterator vector_itr;
-
-            totalItem = bookDataVector.size();
-            totalPage = (int)(totalItem/itemPerPage) + 1;
-            currentPage = 1;
-            string navKey;
-
-            int lower = 0;
-            while(1){
-                clearScreen();
-                lower = ((currentPage-1)) * itemPerPage;
-                cout << "----- Search by Author -----" << endl;
-                cout << "Page " << currentPage << " of " << totalPage << endl;
-                cout << "Search input: " << inputAuthorName << endl;
-                cout << "Total Entries: " << bookDataVector.size() << endl;
-                cout << endl;
-
-                cout << setw(25) << left << "     Title" 
-                    << setw(20) << left << "     Author" 
-                    << setw(20) << left << "     Publish date" 
-                    << setw(11) << left << "     avaialable" 
-                    << endl;
-                
-                vector_itr = bookDataVector.begin();
-                
-            }
 
         }
 };
